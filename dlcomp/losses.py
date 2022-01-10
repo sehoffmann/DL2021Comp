550b6a37a78bcdc82e179218cc8f16ef06a82551
Christian Lessig <lessig@isg.cs.uni-magdeburg.de>
2022-01-10 18:22:18 +0100

    better impl

--- dlcomp/losses.py
+++ dlcomp/losses.py
@@ -20,13 +20,14 @@ class BootstrapLoss(nn.Module):
     def __init__(self, **cfg):
         super(BootstrapLoss, self).__init__()
         self.mse = nn.MSELoss(reduction='none')
-        self.p = torch.nn.Parameter(torch.Tensor([cfg.pop('p')]).to(torch.float32), requires_grad=False)
+        self.p = cfg.pop('p') #torch.nn.Parameter(torch.Tensor([cfg.pop('p')]).to(torch.float32), requires_grad=False)
         self.nan = torch.nn.Parameter(torch.Tensor([float('nan')]).to(torch.float32), requires_grad=False)
     
     def forward(self, input, target):
         mse = self.mse(input * 255, target * 255)  # BCHW
-        mse_pixel = torch.mean(mse, axis=1).flatten(start_dim=1)  # B x H*W
-        median = torch.quantile(mse_pixel, 1-self.p)
-        bootstrapped = torch.where(mse_pixel > median, mse_pixel, self.nan)
-        return torch.nanmean(bootstrapped)
+        mse_pixel = torch.mean(mse, axis=1).flatten()  # B x H*W
+        sorted, _ = torch.sort(mse_pixel)
+        quantile_idx = int((1.-self.p) * sorted.shape[0])
+        upper_part = sorted[quantile_idx:]
+        return torch.mean(upper_part)
 
